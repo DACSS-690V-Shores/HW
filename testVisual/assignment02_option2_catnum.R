88e41d174939196be6295bee0c380a874af4cc8b
Tim Shores <831129+timshores@users.noreply.github.com>
2025-01-10 22:18:43 -0500

    plot play

--- testVisual/assignment02_option2_catnum.R
+++ testVisual/assignment02_option2_catnum.R
@@ -38,6 +38,13 @@ arrests$Age[is.na(arrests$Age)] <- median(arrests$Age, na.rm = TRUE)
   # set NA Arrest Types to "Unknown"
 arrests$Arrest_Type[is.na(arrests$Arrest_Type)] <- "Unknown"
 
+# Replace values in the column
+arrests$Arrest_Type <- recode(arrests$Arrest_Type,
+                              "F" = "Felony",
+                              "O" = "Other",
+                              "M" = "Misdemeanor",
+                              "W" = "Warrant")
+
   # Boxplot
 baseArrest = ggplot(data = arrests,
                     aes(x = Arrest_Type, 
@@ -61,14 +68,39 @@ jitterMEANs = baseMEANs + geom_jitter(color = "#009292",
   # when the notches between two groups don't overlap then
   # it suggests medians are significantly different
   # 
-jitterMEANs = jitterMEANs + geom_point(stat = "summary") +
+jitterMEANs = jitterMEANs + 
   geom_boxplot(alpha = 0.2, # Adjust alpha for transparency
                fill = "#FFFF6D",
                color = "#924900",  
                linewidth = 1,
                notch = TRUE,
-               notchwidth = 0.75,)
+               notchwidth = 0.75) +
+    # use μ to indicate where mean is
+  geom_text(stat = "summary", 
+            label = "μ",
+            color = "#924900",
+            size = 4,
+            fontface = "bold")
 
-jitterMEANs 
 
+# Load up plot information variables
+titleText = 'Arrests centered on 30-40 year olds'
+sub_titleText = 'Misdemeanor and warrant arrests lead in Massachusetts 2019-2020'
+sourceText = 'Source: Massachusetts State Police'
+x.AxisText = 'Arrest Type'
+y.AxisText = 'Age of Arrestee'
+legTitle = 'Greater than \nstatewide mean'
+meanAnnotation = paste("Statewide STR mean =", round(meanSTRatio, 2))
 
+# Decorate with contextual info
+jitterMEANs = jitterMEANs + 
+  labs(title = titleText,
+       subtitle = sub_titleText,
+       x = x.AxisText,
+       y = y.AxisText, 
+       caption = sourceText,
+       colour = legTitle) +
+  theme(plot.caption = element_text(hjust = 0),
+        plot.title = element_text(hjust = 0),
+        legend.title = element_text(size = ),
+        legend.text = element_text(size = 8))
